cb5b1f124c5c48ce84f4b384530469b9204c8ea5
cachemedei <cachemedei@gmail.com>
2025-01-21 18:51:33 +0800

    created workshops page, added search bar to workshop page for notes

--- src/pages/Workshop.jsx
+++ src/pages/Workshop.jsx
@@ -4,9 +4,22 @@ import { notes } from '../utils/notes-data';
 const Workshop = () => {
     return (
         <main className='md:mt-8 md:ml-48 lg:ml-52 xl:ml-60 font-main'>
-            <h1 className='text-2xl md:text-3xl pl-5 mb-2'>{workshops[0].title}</h1>
-            <h3 className='text-xl md:text-2xl pl-6 mb-8 md:mb-1'>{workshops[0].organisation}</h3>
-            <p className='w-4/5 mx-auto font-light md:text-lg lg:w-3/5'>{workshops[0].description}</p>
+            <h1 className='text-2xl md:text-3xl pl-5 mb-2'>
+                {workshops[0].title}
+            </h1>
+            <h3 className='text-xl md:text-2xl pl-6 mb-8 md:mb-1'>
+                {workshops[0].organisation}
+            </h3>
+            <p className='w-4/5 mx-auto font-light md:text-lg lg:w-3/5'>
+                {workshops[0].description}
+            </p>
+            <div className='space-x-4 w-fit mx-auto flex flex-col'>
+                <label htmlFor='search-workshops' className='font-light'>Search Notes:</label>
+                <input
+                    className='bg-pink-light/30 rounded p-2 w-72'
+                    type='search'
+                />
+            </div>
             <section className='grid grid-cols-1 w-fit mx-auto my-4 gap-2 md:grid-cols-2 md:gap-6 lg:grid-cols-3'>
                 {notes.map((note) => {
                     return (
--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -1,6 +1,47 @@
+import { Link } from 'react-router-dom';
+import { workshops } from '../utils/workshops';
+
 const Workshops = () => {
-  return (
-    <div>Workshops Page</div>
-  )
-}
-export default Workshops
+    const truncateString = (str, num) => {
+        if (str?.length > num) {
+            return str.slice(0, num) + `...`;
+        } else return str;
+    };
+    return (
+        <main className='font-main md:mt-8 md:ml-48 lg:ml-52'>
+            {/*  className='md:mt-8 md:ml-48 lg:ml-52 xl:ml-60' */}
+            <h1 className='text-2xl'>All Workshops</h1>
+            <div className='space-x-4 w-fit mx-auto'>
+                <label htmlFor='search-workshops'>Search</label>
+                <input
+                    className='bg-blue-light/30 rounded p-2 w-72'
+                    type='search'
+                />
+            </div>
+            <section className='font-main p-2 w-full gap-8 space-y-4 lg:w-fit lg:mx-auto'>
+                {workshops.map((workshop) => {
+                    return (
+                        <article
+                            key={workshop.id}
+                            className='px-6 py-8 space-y-4 bg-blue-light/50 rounded shadow-blue-dark shadow-md md:w-[4/5] lg:w-[650px]'
+                        >
+                            <h3 className='text-2xl font-accent tracking-wider pl-2'>
+                                {workshop?.title}
+                            </h3>
+                            <h4 className='pl-4 text-xl'>
+                                {workshop.organisation}
+                            </h4>
+                            <p className='font-light text-lg px-4'>
+                                {truncateString(workshop?.description, 240)}
+                            </p>
+                            <div className='w-fit ml-auto font-accent text-2xl border-[1px] border-blue-dark/40 hover:border-blue-dark duration-300 ease-in-out px-3 py-1 rounded'>
+                                <Link to='/workshop'>View</Link>
+                            </div>
+                        </article>
+                    );
+                })}
+            </section>
+        </main>
+    );
+};
+export default Workshops;

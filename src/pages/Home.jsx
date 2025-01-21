7565e013c6ca7a8e32c3588993086bb0c24d53f5
cachemedei <cachemedei@gmail.com>
2025-01-21 11:52:15 +0800

    added workshops dummy data, created and styled recent workshops component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import RecentWorkshops from '../components/RecentWorkshops';
 
 const Home = () => {
     return (
-        <main className='md:mt-8 md:ml-44 lg:ml-52 xl:ml-60'>
+        <main className='md:mt-8 md:ml-48 lg:ml-52 xl:ml-60'>
             <About />
             <RecentWorkshops />
         </main>

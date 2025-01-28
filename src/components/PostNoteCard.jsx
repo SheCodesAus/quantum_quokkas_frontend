27e3b8528eaeb08cead11bed8b0981f9a99d822b
cachemedei <cachemedei@gmail.com>
2025-01-28 11:04:30 +0800

    continued work on styles

---
+++ src/components/PostNoteCard.jsx
@@ -0,0 +1,35 @@
+const PostNoteCard = ({ notes }) => {
+    return (
+        <section className='grid grid-cols-1 w-fit mx-auto my-4 gap-2 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-3 xl:grid-cols-4'>
+            {notes?.notes.map((note) => {
+                return (
+                    <article
+                        key={note.id}
+                        className='bg-note bg-cover size-52 lg:size-64 flex justify-center'
+                    >
+                        <p
+                            className={`font-note tracking-wide ml-[-8px] w-44 h-fit my-auto ${
+                                // less than 20 char
+                                note.content.length <= 20
+                                    ? 'text-4xl'
+                                    : // more than 20 but less than 40 char
+                                    note.content.length > 20 &&
+                                      note.content.length <= 40
+                                    ? 'text-2xl'
+                                    : // more than 40 but less than 60 char
+                                    note.content.length > 40 &&
+                                      note.content.length <= 60
+                                    ? 'text-xl'
+                                    : // more than 60 char
+                                      'text-lg pt-5'
+                            }`}
+                        >
+                            {note.content}
+                        </p>
+                    </article>
+                );
+            })}
+        </section>
+    );
+};
+export default PostNoteCard;
